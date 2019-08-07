9a964ecf01b1d2c7c6a509aeda5d482e9bc17da9
Zac Smith <zrs3141592@gmail.com>
2019-08-06 19:52:54 -0600

    add express and set up template

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// implement your API here
+const express = require("express");
+
+// other files
+
+// global objects
+const server = express();
+const port = 4001;
+
+// middleware
+
+// request handlers
+
+// GET /api/users
+
+// GET /api/users/:id
+
+// POST /api/users
+
+// DELETE /api/users/:id
+
+// PUT /api/users/:id
