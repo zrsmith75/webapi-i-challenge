4b59e81dc977421f9787a578c50715f1c87bceb8
Zac Smith <zrs3141592@gmail.com>
2019-08-06 20:16:44 -0600

    create get /api/users

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require("express");
 
 // other files
+const db = require("./data/db.js");
 
 // global objects
 const server = express();
 const port = 4001;
 
 // middleware
+server.use(express.json());
 
-// request handlers
+// request handlers ***********
 
 // GET /api/users
+// Test GET connection
+// server.get("/api/users", (req, res) => {
+//   res.send("Testing API and /api/users");
+// });
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The users information could not be retrieved."
+      });
+    });
+});
 
 // GET /api/users/:id
 
 // POST /api/users
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  console.log("new user", newUser);
+  db.add(newUser)
+    .then()
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
 
 // DELETE /api/users/:id
 
 // PUT /api/users/:id
+
+// LISTEN
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port} ...`);
+});
